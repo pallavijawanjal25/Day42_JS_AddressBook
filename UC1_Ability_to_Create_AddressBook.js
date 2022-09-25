5ee2c6fced428bc79ef250ccf7c029ca371d56f0
Pallavi <109516737+pallavijawanjal25@users.noreply.github.com>
2022-09-25 20:53:20 +0530

    Added UC1_Ability_to_Create_AddressBook

---
+++ UC1_Ability_to_Create_AddressBook.js
@@ -0,0 +1,27 @@
+class Contact{
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    eMail;
+
+    constructor(...params){
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNumber = params[6];
+        this.eMail = params[7];
+    }
+
+        toString(){
+            return ("Name: "+this.firstName+this.lastName+" Address: "+this.address+"City: "+this.city+"State: "+this.state+"Zip Code: "+this.zip+" Mobile: "+this.phoneNumber+ " EMail: "+this.eMail);
+        }
+}
+let contactObj = new Contact("Pallavi","J","Bangalore","Bangalore","Karnataka",560027,9945007207,"pallavijawanjal@gmail.com");
+console.log(contactObj.toString());
